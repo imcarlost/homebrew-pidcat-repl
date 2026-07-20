3639eb837a0e98997e9d30c52190eb45005fff93
Carlos Martino <soy.carlosmartino@gmail.com>
2026-07-20 11:44:58 -0400

    Update pidcat-repl to 2.4.0

--- Formula/pidcat-repl.rb
+++ Formula/pidcat-repl.rb
@@ -1,8 +1,8 @@
 class PidcatRepl < Formula
   desc "Colored logcat filtered by application package, with an interactive filter UI"
   homepage "https://github.com/imcarlost/pidcat-repl"
-  url "https://github.com/imcarlost/pidcat-repl/archive/refs/tags/v2.3.0.tar.gz"
-  sha256 "a677ce3e0058597a89cd54c45500124ba77a29bd2033a9005ff34cccbeb56fad"
+  url "https://github.com/imcarlost/pidcat-repl/archive/refs/tags/v2.4.0.tar.gz"
+  sha256 "2e6aed994ffb2517cc37fa4bf66bce0a5dbb368d2c34dd389e4479b2234c175c"
   license "Apache-2.0"
 
   depends_on "python3"
